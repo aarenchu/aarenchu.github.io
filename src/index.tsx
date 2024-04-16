0a91417600b696750afbe0517882ae5952197426
Aaren Chu <aarenchu@gmail.com>
2024-04-16 15:09:49 -0400

    info updates and change to TypeScript

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,15 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
+import React from 'react';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
-  // <React.StrictMode>
-  <Router basename={process.env.PUBLIC_URL}>
-    <App />
-  </Router>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <Router basename={process.env.PUBLIC_URL}>
+      <App />
+    </Router>
+  </React.StrictMode>
 );
 registerServiceWorker();
